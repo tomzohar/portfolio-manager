bd7a5086de37191ec2d974c07ec31138c30df4ac
Tom Zohar <60091317+tomzohar@users.noreply.github.com>
2025-11-29 11:40:39 +0200

    feat: initialize angular frontend with nx, material, and ngrx (#5)

---
+++ frontend/client/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
+};
